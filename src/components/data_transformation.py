0618fc9808a073f04748a53552570e6eeca9d2e6
Sanskriti0204 <sanskritiy7@gmail.com>
2025-04-05 11:42:47 +0530

    Model Training done

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -10,7 +10,6 @@ from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from src.exception import CustomException
 from src.logger import logging
 from src.utils import save_object
-from src.components.data_ingestion import DataIngestionConfig,DataIngestion
 
 
 @dataclass
@@ -99,9 +98,6 @@ class DataTransformation:
             raise CustomException(e, sys)
 
 
-if __name__=="__main__":
-    obj=DataIngestion()
-    train_data,test_data=obj.initiate_data_ingestion()
-    data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+
+
             
